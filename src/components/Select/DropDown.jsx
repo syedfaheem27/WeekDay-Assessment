81b5c65356b69221d0544ee9c5adb7d4b9068fdb
syedfaheem27 <syedfaheem2719@gmail.com>
2024-05-06 11:29:46 +0530

    Add filter pills and make the custom select more reusable: Add filter pills which appear on selecting filters and make the custome select reusable in terms of making selections

--- src/components/Select/DropDown.jsx
+++ src/components/Select/DropDown.jsx
@@ -3,11 +3,11 @@ import styles from "./DropDown.module.css";
 
 //Filters will come down as a prop
 //which makes it reusable
-const DropDown = ({ filters }) => {
+const DropDown = ({ filters, onSelect }) => {
     return (
         <div className={styles["drop-down"]}>
-            {filters.map((criteria, index) => {
-                return <span key={criteria + index}>{criteria}</span>;
+            {filters.map((filter, index) => {
+                return <span onClick={() => onSelect(filter)} key={filter + index}>{filter}</span>;
             })}
         </div>
     );
--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,35 +1,67 @@
-import { useState } from "react"
-import Wrapper from "./Wrapper"
-import DropDown from "./DropDown"
+import { useState } from "react";
+import Wrapper from "./Wrapper";
+import DropDown from "./DropDown";
 
-const DummyFilters = [
-    "India",
-    "USA",
-    "AFG",
-    "AUS"
-]
+const DummyFilters = ["India", "USA", "AFG", "AUS"];
 
-
-const Select = () => {
-    const [isOpen, setIsOpen] = useState(false) /* defines whether drop down is open or not */
+/* isSingle is a prop that decides if we're able to make a single selection or multiple selections
+as we have minimum experience and min base salary which are single field selections */
+const Select = ({ isSingle }) => {
+    const [isOpen, setIsOpen] =
+        useState(false); /* defines whether drop down is open or not */
+    const [selectedFilters, setSelectedFilters] = useState(
+        []
+    ); /* holds all the selected filters which will be shown as chips*/
 
     const handleToggleDropDown = () => {
-        setIsOpen(op => !op);
-    }
+        setIsOpen((op) => !op);
+    };
 
     const handleCloseDropDown = () => {
-        setIsOpen(false)
-    }
+        setIsOpen(false);
+    };
 
+    const handleSelectFilters = (val) => {
+        if (isSingle) {
+            return setSelectedFilters([val]);
+        }
 
+        setSelectedFilters((prev) => {
+            if (prev.includes(val)) return prev;
+
+            return [...prev, val];
+        });
+    };
+
+    console.log(selectedFilters);
 
     return (
         <Wrapper onClick={handleToggleDropDown}>
+            {isOpen && (
+                <DropDown filters={DummyFilters} onSelect={handleSelectFilters} />
+            )}
             {
-                isOpen && <DropDown filters={DummyFilters} />
+                /* if there are selected filters, we would want to show filter chips */
+
+                selectedFilters.length !== 0 && (
+                    <div
+                        style={{
+                            position: "absolute",
+                            top: "50%",
+                            transform: "translateY(-50%)",
+                            display: 'flex',
+                            gap: '4px',
+                            alignItems: 'center',
+                        }}
+                    >
+                        {selectedFilters.map((filter, index) => {
+                            return <span key={filter + index}>{filter}</span>;
+                        })}
+                    </div>
+                )
             }
         </Wrapper>
-    )
-}
+    );
+};
 
-export default Select
+export default Select;
