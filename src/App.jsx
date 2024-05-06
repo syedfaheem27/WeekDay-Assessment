581e46db3bf481eb9f45b0af006884ae32277220
syedfaheem27 <syedfaheem2719@gmail.com>
2024-05-06 13:47:27 +0530

    Add logic for infinite loading

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Select from "./components/Select/Select";
 import { loadMoreData } from "./slices/dataSlice";
 import Card from "./components/Card/Card";
+import { useEffect, useRef } from "react";
 /*
 Filters that we need to implement
 
@@ -25,6 +26,46 @@ function App() {
     (store) => store.data
   );
   const dispatch = useDispatch();
+
+  const ref = useRef(null);
+
+
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px 0px 0px 0px",
+      threshold: 0.7,
+    };
+
+    const obsCallback = (entries, observer) => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+
+
+        console.log("Intersecting")
+
+        observer.unobserve(entry.target);
+      });
+    };
+    const observer = new IntersectionObserver(
+      obsCallback,
+      options
+    );
+    let copy_ref;
+    if (ref.current) {
+      copy_ref = ref.current;
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (copy_ref) observer.unobserve(copy_ref);
+    };
+  }, [data]);
+
+
+
+
   return (
     <>
       <button onClick={() => dispatch(loadMoreData())}>Click</button>
@@ -35,6 +76,15 @@ function App() {
 
 
       <Card />
+
+      {
+        /*This will hold as a trigger for infinte loading*/
+      }
+      <footer ref={ref} style={{
+        margin: '32px',
+        border: '1px solid red',
+        height: '120px'
+      }}></footer>
     </>
   );
 }
